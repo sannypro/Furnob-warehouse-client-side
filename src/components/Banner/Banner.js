61f3a8f2e813e26e19aabb046df78397d43c4e1f
sannypro <sannymr79@gmail.com>
2022-04-29 00:19:14 +0600

    header and banner done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import bannerImg from "../../assets/img/banner-image-22-removebg-preview.png"
+import bannerImg from "../../assets/img/banner-image-22-removebg-preview.png";
+import bannerImg2 from "../../assets/img/shop-img24-removebg-preview.png"
 import './Banner.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { Carousel, CarouselItem } from 'react-bootstrap';
 
 const Banner = () => {
     return (
         <div className='bg-color'>
-            <div className='d-flex justify-content-between  align-items-center container'>
+            <div className='d-flex justify-content-center  align-items-center container'>
                 <div>
                     <small>FURNITURE</small>
-                    <Swiper
-                        spaceBetween={50}
-                        slidesPerView={3}
-                        onSlideChange={() => console.log('slide change')}
-                        onSwiper={(swiper) => console.log(swiper)}>
 
-                        <SwiperSlide><h1>Beauty indoors</h1>
-                            <p>The perfect place for every contemporay durniture and manufacturer.This is Furnob Warehouse</p></SwiperSlide>
-                        <SwiperSlide><h1>Better interior</h1>
-                            <p>The perfect place for every contemporay durniture and manufacturer.This is Furnob Warehouse</p>
-                        </SwiperSlide>
+                    <div className=''>
+                        <h1>Beauty indoors</h1>
+                        <p className='w-50'> The perfect place for every contemporay durniture and manufacturer.This is Furnob Warehouse</p>
+                    </div>
 
-                        ...
-                    </Swiper>
 
 
                 </div>
                 <div>
-                    <img src={bannerImg} alt="" />
+                    <Carousel>
+                        <Carousel.Item>
+                            <img src={bannerImg} alt="" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img src={bannerImg2} alt="" />
+                        </Carousel.Item>
+                    </Carousel>
+
                 </div>
             </div>
         </div>
